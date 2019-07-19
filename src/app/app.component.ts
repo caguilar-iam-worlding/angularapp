fae4fef4c3da92943760107e292defd80692cb87
Carlos Aguilar Miguel <cagmiteleco@gmail.com>
2019-07-19 12:38:12 +0200

    Create alert component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iamworldingangularapp';
-  city = 'Madrid';
+
+  // Data to HTML template from server
+  app = {
+    title: 'What\'s up',
+    subtitle: 'Look what is happening around you and find plans, things to do and enjoy!',
+    action: 'Find a plan!'
+  };
+  // Data from User service when reload the page
+  user = {
+    actual_location: 'Madrid',
+    is_facebook_profile_upload: true,
+    is_twitter_profile_upload: true,
+    is_instagram_profile_upload: false,
+  };
+  // From Data service where reload the page
+  plans = [
+    { name: 'Parque del Retiro', address: 'Avda. independencia', latitude: 40.411214, longitude: -3.713434 },
+    { name: 'Parque del Almendro', address: 'Avda. grande', latitude: 40.40212, longitude: -3.73423948 }
+  ];
+
+  on_action() {
+    console.log('Click on action button');
+  }
+
+  show_alert() {
+    if (!this.user.is_facebook_profile_upload || !this.user.is_instagram_profile_upload || !this.user.is_twitter_profile_upload) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './elements/header/header.component';
 import { FooterComponent } from './elements/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
+import { AlertComponent } from './elements/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule
