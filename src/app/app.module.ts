de424bc6a0053922cf68f935d2bf0bc2622ee464
Carlos Aguilar Miguel <cagmiteleco@gmail.com>
2019-07-19 13:01:20 +0200

    Add FormsModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AlertComponent } from './elements/alert/alert.component';
     AlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
